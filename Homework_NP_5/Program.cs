446b8c01f42ca1ff920ff0633292bf2868e0e28b
Hacha_tq55@student.itstep.org <Hacha_tq55@student.itstep.org>
2024-10-22 20:49:29 +0300

    Добавьте файлы проекта.

---
+++ Homework_NP_5/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Net;
+
+namespace Homework_NP_5
+{
+    public class Program
+    {
+        static void Main()
+        {
+            string urlString = "https://cdn.pixabay.com/photo/2024/02/09/23/02/trees-8563877_1280.jpg";
+            string fileCopy = @"C:\html\image.jpg";
+
+            WebClient client = new WebClient();
+
+            using (Stream stream = client.OpenRead(urlString))
+            {
+                using (FileStream fileStream = File.Create(fileCopy))
+                {
+                    stream.CopyTo(fileStream);
+                }
+            }
+
+            Console.WriteLine("The file was successfully saved to the path: " + fileCopy);
+        }
+    }
+}
